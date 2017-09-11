96c7fe1fc286208fb80d6ca705725cfbe4593d55
Enrick3344 <enrico3815@hotmail.fr>
2017-09-10 22:19:18 -0400

    Created Main.php

---
+++ src/FantasyHunger/Main.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace FantasyHunger;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+use pocketmine\level\Level;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\Player;
+
+class Main extends PluginBase implements Listener {
+
+}
